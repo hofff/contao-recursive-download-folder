a75b5bc107e85de9d001f3dcbb16ca253446bfcc
David Molineus <david.molineus@netzmacht.de>
2019-02-14 15:15:37 +0100

    Optimize assets.

--- src/Resources/public/js/recursive-download-folder.js
+++ src/Resources/public/js/recursive-download-folder.js
@@ -9,7 +9,7 @@ function toggleClass(element, className){
     }
 
     var classString = element.className, nameIndex = classString.indexOf(className);
-    if (nameIndex == -1) {
+    if (nameIndex === -1) {
         classString += ' ' + className;
     }
     else {
