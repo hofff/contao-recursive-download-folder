c8f07fdf095e7c0a150d43076c8872bb099da119
David Molineus <david.molineus@netzmacht.de>
2019-03-06 15:01:36 +0100

    Add german translation.

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -4,3 +4,4 @@ declare(strict_types=1);
 
 $GLOBALS['TL_LANG']['MSC']['recursiveDownloadFolderKeywordLabel'] = 'Suchbegriff';
 $GLOBALS['TL_LANG']['MSC']['recursiveDownloadFolderSearchLabel']  = 'Suchen';
+$GLOBALS['TL_LANG']['MSC']['recursiveDownloadFolderResetLabel']   = 'Zurücksetzen';
